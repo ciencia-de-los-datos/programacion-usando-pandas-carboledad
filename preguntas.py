9714f16c15df430fc102d240e2996a42501ad913
carboledad <101777132+carboledad@users.noreply.github.com>
2022-04-15 15:08:24 -0500

    Pregunta12
    Pregunta12

--- preguntas.py
+++ preguntas.py
@@ -167,6 +167,7 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
+    tbl0 = pd.read_csv("tbl0.tsv", sep="\t")
     tbl0_orden = tbl0.sort_values(by='_c2')
     tbl0_list = tbl0_orden.groupby(['_c1'])['_c2'].apply(lambda x: ':'.join(str(i) for i in x))
     tbl0_list = tbl0_list.reset_index()
@@ -211,6 +212,7 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
+    tbl2 = pd.read_csv("tbl2.tsv", sep="\t")
     tbl2 = tbl2.sort_values('_c5a')
     tbl2['_c5b'] = tbl2['_c5b'].map(lambda x: str(x))
     tbl2['_c5c'] = tbl2['_c5a'] + ':' + tbl2['_c5b']
